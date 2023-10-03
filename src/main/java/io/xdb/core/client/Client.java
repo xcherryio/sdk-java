cac9406b5c711afdf31b8d322d3a5422b35cd125
Kaili Zhu <kzhu@indeed.com>
2023-10-03 09:03:46 +0900

    add method

--- src/main/java/io/xdb/core/client/Client.java
+++ src/main/java/io/xdb/core/client/Client.java
@@ -22,16 +22,36 @@ public class Client {
         this.basicClient = new BasicClient(clientOptions);
     }
 
+    public String startProcess(final Process process, final String processId, final Object input) {
+        final String processType = ProcessUtil.getProcessType(process);
+        return startProcessInternal(processType, processId, input, process.getOptions());
+    }
+
+    /**
+     * Caution: if you intend to override certain process options, utilize the {@link Client#startProcess(Process, String, Object)} method
+     *
+     * @param processClass
+     * @param processId
+     * @param input
+     * @return
+     */
     public String startProcess(
-        final Process process,
+        final Class<? extends Process> processClass,
+        final String processId,
+        final Object input
+    ) {
+        final String processType = ProcessUtil.getProcessType(processClass);
+        return startProcessInternal(processType, processId, input, null);
+    }
+
+    private String startProcessInternal(
+        final String processType,
         final String processId,
         final Object input,
         final ProcessOptions processOptions
     ) {
         AsyncStateConfig asyncStateConfig = null;
         String startingStateId = "";
-
-        final String processType = ProcessUtil.getProcessType(process);
 
         final Optional<AsyncState> startingState = registry.getProcessStartingState(processType);
         if (startingState.isPresent()) {
